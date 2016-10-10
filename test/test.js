99cfb6cdc109f821532c6ee4b938288ea59e69b1
Sébastien Dussaut <sebastien@switch-company.com>
2016-10-10 17:57:11 +0200

    Added first unitary test

--- test/test.js
+++ test/test.js
@@ -5,34 +5,57 @@
 const test = require( 'tape' ),
       Nightmare = require( './nightmare' ),
       nightmare = new Nightmare( {
-        show: false
+        // show: true
       } );
 
 nightmare.goto( 'http://localhost:3000' );
 
-test( 'modal is open', t => {
+
+// Label test suite in output
+test( '-------------------------------', ( t ) => {
+  t.comment( 'Running *Modal* test suite.' );
+  t.comment( '-------------------------------' );
+  t.end();
+});
+
+// test 1
+test( '1| Le conteneur de la modale doit avoir la valeur « false » pour l’attribut « aria-hidden ».', ( t ) => {
   nightmare.refresh()
     .click( '[data-open=".modal1"]' )
-    .evaluate( () => {
-      return !!document.querySelector( '.modal-wrapper' );
-    } )
-    .then( is => {
-      t.ok( is );
+    .evaluate(() => {
+      var modal = document.querySelector( '.modal-wrapper' );
+      return modal.getAttribute('aria-hidden');
+    })
+    .then(( ariaHidden ) => {
+      t.equal( ariaHidden, 'false', '« aria-hidden » doit valoir « false ».' );
       t.end();
-    } );
-} );
-
-
-test( 'modal is closed', t => {
-  nightmare.refresh()
-    .evaluate( () => {
-      return !!document.querySelector( '.modal-wrapper' );
-    } )
-    .then( isNot => {
-      t.notOk( isNot );
-      t.end();
-    } );
+    });
 });
+
+// test( 'modal is open', t => {
+//   nightmare.refresh()
+//     .click( '[data-open=".modal1"]' )
+//     .evaluate( () => {
+//       return !!document.querySelector( '.modal-wrapper' );
+//     } )
+//     .then( is => {
+//       t.ok( is );
+//       t.end();
+//     });
+// } );
+
+
+// test( 'modal is closed', t => {
+//   nightmare.refresh()
+//     .evaluate( () => {
+//       return !!document.querySelector( '.modal-wrapper' );
+//     } )
+//     .then( isNot => {
+//       t.notOk( isNot );
+//       t.end();
+//     } );
+// });
+
 
 test( '-------------------------------', ( t ) => {
   t.comment( 'Test suite done' );
@@ -40,6 +63,6 @@ test( '-------------------------------', ( t ) => {
   nightmare.end()
   .then( () => {
     t.end();
-  } );
+  });
 });
 
